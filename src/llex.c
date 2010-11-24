8097af40fb4c675fcf5c257432db2710af86711e
Tony Finch <dot@dotat.at>
2010-11-24 02:38:44 +0000

    llex: permit underscores in numbers

--- src/llex.c
+++ src/llex.c
@@ -194,12 +194,14 @@ static void trydecpoint (LexState *ls, SemInfo *seminfo) {
 static void read_numeral (LexState *ls, SemInfo *seminfo) {
   lua_assert(isdigit(ls->current));
   do {
-    save_and_next(ls);
-  } while (isdigit(ls->current) || ls->current == '.');
+    if (ls->current == '_') next(ls);
+    else save_and_next(ls);
+  } while (isdigit(ls->current) || ls->current == '_' || ls->current == '.');
   if (check_next(ls, "Ee"))  /* `E'? */
     check_next(ls, "+-");  /* optional exponent sign */
   while (isalnum(ls->current) || ls->current == '_')
-    save_and_next(ls);
+    if (ls->current == '_') next(ls);
+    else save_and_next(ls);
   save(ls, '\0');
   buffreplace(ls, '.', ls->decpoint);  /* follow locale for decimal point */
   if (!luaO_str2d(luaZ_buffer(ls->buff), &seminfo->r))  /* format error? */
